d26cca2a19f6af1de32a0ba08606bbb841baaf3a
OA Wu <comdan66@gmail.com>
2017-03-29 11:45:06 +0800

    Fix code.

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -9,7 +9,9 @@ class Search extends Site_controller {
 
   public function index ($keywords = '') {
     $keywords = OAInput::get ('keywords') ? OAInput::get ('keywords') : ($keywords ? $keywords : '');
-
+echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
+var_dump ();
+exit ();
     if (!$keywords = trim ($keywords))
       return redirect_message (array (''), array ('_flash_info' => '找不到該關鍵字！'));
 
