033e91d8ea466482b3ab1d9877f7efbd4732398c
OA Wu <comdan66@gmail.com>
2017-08-24 11:43:41 +0800

    Fix code.

---
+++ application/migrations/011_add_ip2cs.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+class Migration_Add_ip2cs extends CI_Migration {
+  public function up () {
+    $this->db->query (
+      ""
+    );
+  }
+  public function down () {
+    $this->db->query (
+      ""
+    );
+  }
+}
